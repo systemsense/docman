1e52ba91bfca00034df3966d10395a3489bac78c
Systemsense <60867570+systemsense@users.noreply.github.com>
2021-03-02 21:36:46 +1100

    Create docman.js

---
+++ docman.js
@@ -0,0 +1,70 @@
+function writeContent(content)
+{
+  // preappend data
+  document.write("<body><base target=\"_top\"><style>#loadingbar{display:none !important;}</style><div id=\"page-wrapper\"><div id=\"content-all\">");
+  var output = ""
+  output += "<span><img src=\"https:\/\/systemsense.github.io\/docman\/images\/icon_back.png\">&nbsp;&nbsp;<a href=\"../\">Up One Folder<\/a></span><br>"  
+  for (i = 0; i < content.items.length; i++) {
+    var thisItem = content.items[i];
+    if(thisItem.type == "folder")
+    {     
+      output += "<span><img src=\"https:\/\/systemsense.github.io\/docman\/images\/icon_folder.png\">&nbsp;&nbsp;<a href=\""+thisItem.encodedName+"/\">"+thisItem.name+"<\/a></span><br>");}
+    }
+    if(thisItem.type == "file")
+    {
+//      output += "<span><img src=\"https:\/\/systemsense.github.io\/docman\/images\/icon_file.png\">&nbsp;&nbsp;<a href=\""+thisItem.encodedName+"/\">"+thisItem.name+"<\/a></span><br>");}
+    }
+    
+
+  }
+  
+  // post append data
+  document.write("</div></div></body>");
+}
+
+function loadshell()
+{
+	
+	
+	var url = window.location.pathname.substring(1);
+
+	try
+	{
+	var filename = url.substring(url.lastIndexOf('/')+1);
+	}
+	catch(err){filename = "index.html"}
+
+
+	var provider = decodeURI(url.split("/")[4])
+	document.write("<head><title>System Sense DOCMAN</title><meta http-equiv=\"X-UA-Compatible\" content=\"IE=Edge\" />")
+	document.write("<meta name=\"viewport\" content=\"initial-scale=1, minimum-scale=1, width=device-width\" \/>")
+	document.write("<meta http-equiv=\"content-type\" content=\"text\/html; charset=utf-8\" />")
+/*	document.write("<link href=\"\/community-resources/style.css\" rel=\"stylesheet\" type=\"text\/css\" />")*/
+	document.write("<script src=\"https:\/\/ajax.googleapis.com\/ajax\/libs\/jquery\/1.6.4\/jquery.min.js\" type=\"text\/javascript\"><\/script>");
+	if(url.indexOf("/download/") != -1)
+	{
+		document.write("<i id=\"loadingbar\">Preparing file for download...</i>");	
+	}
+	else
+	{
+		document.write("<i id=\"loadingbar\">Loading folder information...</i>");
+	}
+	
+	document.write("<script src=\"/docman/docman.js\" type=\"text\/javascript\"><\/script>");
+	
+	if(provider == "sharepoint")
+	{
+	document.write("<script type=\"application\/javascript\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbwj81P-q3z3WYDmOCl99FYtSdzbF9hCVla2WqSGGHhJYmO_RDDmZo0\/exec?lookup="+xstate+"&filename="+filename+"\"><\/script><\/head>");			
+	
+	}
+	else
+	{
+	document.write("<script type=\"application\/javascript\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbzHz5xfGd3dMFK8sl9gnmOovk-2RL3Bgt-LUE9xGAiiRO8DGtPmbfmT\/exec?lookup="+xstate+"&filename="+filename+"\"><\/script><\/head>");			
+	
+	}
+try
+{
+	document.querySelector("#loadingbar").style.display = "none";
+}
+	catch(err){}
+}
