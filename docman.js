816f51bd6ce896f848662c8b39537d2facbc43c4
Systemsense <60867570+systemsense@users.noreply.github.com>
2021-03-09 16:39:34 +1100

    Update docman.js

--- docman.js
+++ docman.js
@@ -41,7 +41,7 @@ function loadContent()
 	document.write("<html><head><title>System Sense DOCMAN</title><meta http-equiv=\"X-UA-Compatible\" content=\"IE=Edge\" />")
 	document.write("<meta name=\"viewport\" content=\"initial-scale=1, minimum-scale=1, width=device-width\" \/>")
 	document.write("<meta http-equiv=\"content-type\" content=\"text\/html; charset=utf-8\" />")
-/*	document.write("<link href=\"\/community-resources/style.css\" rel=\"stylesheet\" type=\"text\/css\" />")*/
+	document.write("<link href=\"\/s/styles.css\" rel=\"stylesheet\" type=\"text\/css\" />")
 	document.write("<script src=\"/docman/docman.js\" type=\"text\/javascript\"><\/script>");
 	document.write("<script src=\"https:\/\/ajax.googleapis.com\/ajax\/libs\/jquery\/1.6.4\/jquery.min.js\" type=\"text\/javascript\"><\/script></head><body>");
 	if(urlx.indexOf("/download/") != -1)
