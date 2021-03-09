653f29d7225d3408adcdd47d322bcaa9984bd1f5
Systemsense <60867570+systemsense@users.noreply.github.com>
2021-03-09 19:08:34 +1100

    Update docman.js

--- docman.js
+++ docman.js
@@ -44,9 +44,9 @@ function loadContent()
 	document.write("<link href=\"\/s/styles.css\" rel=\"stylesheet\" type=\"text\/css\" />")
 	document.write("<script src=\"/docman/docman.js\" type=\"text\/javascript\"><\/script>");
 	document.write("<script src=\"https:\/\/ajax.googleapis.com\/ajax\/libs\/jquery\/1.6.4\/jquery.min.js\" type=\"text\/javascript\"><\/script></head><body>");
-	if(urlx.indexOf("/download/") != -1)
+	if(urlx.indexOf("/view/") != -1)
 	{
-		document.write("<i id=\"loadingbar\">Preparing file for download...</i>");	
+		//document.write("<i id=\"loadingbar\">Preparing file for download...</i>");	
 	}
 	else
 	{
@@ -59,12 +59,7 @@ function loadContent()
 	{
 		if(urlx.indexOf("/view/") != -1)
 		{
-			document.write("<html><head><title>"+titlex+"</title><meta http-equiv=\"X-UA-Compatible\" content=\"IE=Edge\">");
-			document.write("<meta http-equiv=\"content-type\" content=\"text\/html; charset=utf-8\"><link rel=\"shortcut icon\" type=\"image\/x-icon\" href=\"https:\/\/thsconline.github.io\/s\/images\/icon_pdf2.png\">");
-			document.write("<link href=\"\/s\/styles.css\" rel=\"stylesheet\" type=\"text\/css\">");
-			document.write("<style>html, body {height:100% !important;}</style>");
-			document.write("<script src=\"https:\/\/ajax.googleapis.com\/ajax\/libs\/jquery\/1.6.4\/jquery.min.js\" type=\"text\/javascript\"><\/script>");
-			document.write("<\/head><body>");			
+			document.write("<body>");			
 			document.write("<div id=\"overlaybar\" style=\"z-index:1000\; width:100%;\">"+ unescape(filename) +"<span id=\"overlayinsert\" style=\"float:right !Important\"><!--<a target=\"_blank\" href=\"https://systemsense.github.io/s/d/"+viewno+"/"+titlex+"\" class=\"border\">Download File<\/a>-->&nbsp;&nbsp;<a class=\"border\" href=\"..\/..\/\">Close &#215;</span></div><br>")
 			document.write("<iframe style=\"width:100%; height:96%;\" height=\"96%\" sandbox=\"allow-scripts allow-popups allow-pointer-lock allow-presentation allow-same-origin allow-modals allow-top-navigation allow-downloads\" allowscripts=\"1\" allowdownloads=\"1\" allowfullscreen=\"1\" frameborder=\"0\" id=\"viewer\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbyJcOnas1azziwJ6jr6CedHb6W0hnNlQSnYSW-WzCLdsL79e3M8WzVRKjoNLBM5I3pP\/exec?lookup="+statex+"&filename="+filename+"\"><noscript>&nbsp;Enable Javascript to Load File<\/noscript><\/iframe>");
 			document.write("</body></html>"); 	 
