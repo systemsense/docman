7e1b15af64e61df635dc686fb1acc7b64cda2034
Systemsense <60867570+systemsense@users.noreply.github.com>
2021-03-10 04:37:45 +1100

    Update docman.js

--- docman.js
+++ docman.js
@@ -13,7 +13,16 @@ function writeContent(content)
     }
     if(thisItem.type == "file")
     {
-      output += "<span><img src=\"https:\/\/systemsense.github.io\/docman\/images\/icon_file.png\">&nbsp;&nbsp;<a href=\"view/"+thisItem.encodedRef + "/" + thisItem.name+"\">"+thisItem.name+"<\/a></span><br>"
+      if(thisItem.ext == "doc" || thisItem.ext == "docx" || thisItem.ext == "docxm" || thisItem.ext == "xlsx" || thisItem.ext == "xlsx" || thisItem.ext == "xlsxm" || thisItem.ext == "ppt" || thisItem.ext == "pptx" || thisItem.ext == "pdf" || thisItem.ext == "zip")	      
+	{
+	      output += "<span><img src=\"https:\/\/systemsense.github.io\/docman\/images\/icon_"+thisItem.ext+".png\">&nbsp;&nbsp;<a href=\"view/"+thisItem.encodedRef + "/" + thisItem.name+"\">"+thisItem.name+"<\/a></span><br>"
+	}
+	else
+	{
+		output += "<span><img src=\"https:\/\/systemsense.github.io\/docman\/images\/icon_file.png\">&nbsp;&nbsp;<a href=\"view/"+thisItem.encodedRef + "/" + thisItem.name+"\">"+thisItem.name+"<\/a></span><br>"
+	}
+	    
+      
     }
     
 
